f7066d536fcce088c1b2d9c24166ba38ea3c43a2
Mateus Junior <juniormateusknd@gmail.com>
2023-06-11 20:08:39 -0300

    feat: Add subscribeAll to subscribable.dart

--- lib/mixin/subscribable.dart
+++ lib/mixin/subscribable.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/foundation.dart';
 
-mixin Subscribable<Type extends Enum, Payload> {
-  final Map<Type, List<void Function(Type, Payload)>> _listeners = {};
+mixin Subscribable<T extends Enum, Payload> {
+  final Map<T, List<void Function(T, Payload)>> _listeners = {};
 
-  void subscribe(Type level, void Function(Type, Payload) callback) {
+  void subscribe(T level, void Function(T, Payload) callback) {
     if (_listeners.containsKey(level)) {
       _listeners[level]?.add(callback);
     } else {
@@ -11,19 +11,18 @@ mixin Subscribable<Type extends Enum, Payload> {
     }
   }
 
-  void subscribeAll(void Function(Type, Payload) callback) {
-    dynamic dynamicEnum = Type;
-    for (final level in dynamicEnum.values.toList()) {
+  void subscribeAll(List<T> levels, void Function(T, Payload) callback) {
+    for (final level in levels) {
       subscribe(level, callback);
     }
   }
 
-  void unsubscribe(Type level, void Function(Type, Payload) callback) {
+  void unsubscribe(T level, void Function(T, Payload) callback) {
     _listeners[level]?.remove(callback);
   }
 
   @protected
-  void dispatch(Type messageLevel, Payload message) {
+  void dispatch(T messageLevel, Payload message) {
     for (final level in _listeners.keys) {
       if (messageLevel.index >= level.index) {
         _listeners[level]?.forEach((callback) => callback(messageLevel, message));
