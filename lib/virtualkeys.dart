d379cd52befa4d2d765009d4b5b3269c136b0595
Mateus Junior <mateus@matj.dev>
2024-12-26 14:25:37 +0100

    refactor(win32): consolidate and restructure virtual keys/buttons
    - Removed `button.dart`, `parse_key.dart`, and `virtualkeys.dart`
    - Created `win32/virtualkeys.dart` for virtual key handling
    - Moved ButtonFlags logic to `win32/virtualbuttons.dart`
    - Updated exports in `virtualkeys.dart` to reflect changes

--- lib/virtualkeys.dart
+++ lib/virtualkeys.dart
@@ -1,11 +1,10 @@
 library;
 
-export 'virtualkeys/virtualkeys.dart';
+export 'virtualkeys/win32/virtualkeys.dart';
+export 'virtualkeys/win32/virtualbuttons.dart';
 export 'virtualkeys/protobuf/parse_key_action.dart';
 export 'virtualkeys/protobuf/parse_button_action.dart';
 export 'virtualkeys/button.dart';
 export 'virtualkeys/key.dart';
 export 'virtualkeys/keyboard.dart';
 export 'virtualkeys/exceptions.dart';
-export 'virtualkeys/win32/button.dart';
-export 'virtualkeys/win32/parse_button.dart';
--- lib/virtualkeys/win32/virtualbuttons.dart
+++ lib/virtualkeys/win32/virtualbuttons.dart
@@ -1,4 +1,4 @@
-import 'package:win32/win32.dart' show MOUSE_EVENT_FLAGS;
+import 'package:win32/win32.dart';
 import '../button.dart';
 
 /// Windows has a different way of handling mouse buttons
@@ -53,4 +53,30 @@ class ButtonFlags {
   String toString() {
     return button.toString();
   }
+}
+
+
+Map<Button, int> _mouseButtonToVK = {
+  Button.LEFT: VIRTUAL_KEY.VK_LBUTTON,
+  Button.RIGHT: VIRTUAL_KEY.VK_RBUTTON,
+  Button.MIDDLE: VIRTUAL_KEY.VK_MBUTTON,
+  Button.X1: VIRTUAL_KEY.VK_XBUTTON1,
+  Button.X2: VIRTUAL_KEY.VK_XBUTTON2,
+};
+final Map<int, Button> _vkToMouseButton = _mouseButtonToVK.map((k, v) => MapEntry(v, k));
+
+int mouseButtonToVK(Button button) {
+  if (!_mouseButtonToVK.containsKey(button)) {
+    throw ArgumentError('Invalid mouse button type');
+  }
+
+  return _mouseButtonToVK[button]!;
+}
+
+Button vkToMouseButton(int vk) {
+  if (!_vkToMouseButton.containsKey(vk)) {
+    throw ArgumentError('Invalid virtual key');
+  }
+
+  return _vkToMouseButton[vk]!;
 }
